dc46984730041c63ed2144579e2a8fcd6d0879de
Tobin Waters <tkwaterss@gmail.com>
2023-04-07 10:14:34 -0600

    added some radio button practice

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,29 +1,60 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
   const searchRef = useRef();
   const navigate = useNavigate();
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (searchRef.current.value.length < 6) {
-      return alert("thats too short");
-    } 
-    
-    console.log('its longer than 6')
-    navigate(`/details/${searchRef.current.value}`)
+  const [show, setShow] = useState("");
+
+  const handleRadio = (event) => {
+    setShow(event.target.value);
   };
 
-    return (
-      <div>
-        <h1>HomeScreen</h1>
-        <form onSubmit={handleSubmit}>
-          <input type="text" ref={searchRef} />
-          <button>Search</button>
-        </form>
-      </div>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(show);
+    console.log(searchRef.current.value);
+
+    // console.log('its longer than 6')
+    // navigate(`/details/${searchRef.current.value}`)
   };
+
+  return (
+    <div>
+      <h1>HomeScreen</h1>
+      <h2>Whats Your Favorite Show</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={searchRef} />
+
+        <input
+          onChange={handleRadio}
+          id="show1"
+          value="the-office"
+          name="show"
+          type="radio"
+        />
+        <label htmlFor="show1">The Office</label>
+        <input
+          onChange={handleRadio}
+          id="show2"
+          value="silicon-valley"
+          name="show"
+          type="radio"
+        />
+        <label htmlFor="show2">Silicon Valley</label>
+        <input
+          onChange={handleRadio}
+          id="show3"
+          value="peaky-blinders"
+          name="show"
+          type="radio"
+        />
+        <label htmlFor="show3">Peaky Blinders</label>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
 
 export default HomeScreen;
